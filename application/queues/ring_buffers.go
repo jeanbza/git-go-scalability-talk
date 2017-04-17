21875c26f3a3d49ca7b5ba830a97f75ad77c42f6
Jean de Klerk <jadekler@gmail.com>
2017-04-16 18:34:08 -0600

    Start work on ringbuffer

--- application/queues/ring_buffers.go
+++ application/queues/ring_buffers.go
@@ -1,13 +1,42 @@
 package queues
 
-type RingBuffer struct {
+import "sync"
 
+type RingBuffer struct {
+    buffer       [][]byte
+    inputCursor  int
+    outputCursor int
+    mu           *sync.Mutex
+}
+
+func NewRingBuffer(size int) *RingBuffer {
+    return &RingBuffer{
+        buffer:       make([][]byte, size),
+        inputCursor:  0,
+        outputCursor: 0,
+    }
 }
 
 func (q *RingBuffer) Enqueue(data []byte) {
+    q.mu.Lock()
+    q.buffer[q.inputCursor] = data
 
+    if (q.inputCursor == len(q.buffer) - 1) {
+        q.inputCursor = -1
+    }
+    q.inputCursor++
+    q.mu.Unlock()
 }
 
 func (q *RingBuffer) Dequeue() []byte {
-    return nil
+    q.mu.Lock()
+    data := q.buffer[q.outputCursor]
+
+    if (q.outputCursor == len(q.buffer) - 1) {
+        q.outputCursor = -1
+    }
+    q.outputCursor++
+    q.mu.Unlock()
+
+    return data
 }
